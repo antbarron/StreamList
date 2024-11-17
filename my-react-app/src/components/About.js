db293d523194ad1e31888e214eaec5e789afe687
antbarron <162982520+antbarron@users.noreply.github.com>
2024-11-16 21:39:18 -0600

    Update About.js

--- my-react-app/src/components/About.js
+++ my-react-app/src/components/About.js
@@ -1,13 +1,17 @@
-// src/About.js
 import React from 'react';
 
 const About = () => {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>This page will be built in Week 5.</p>
+      <h1>About the Company</h1>
+      <ul>
+        <li>EZTechMovie is a privately owned video streaming company with its headquarters and data located in San Diego, California. Pat Jones, the Founder and CEO, established EZTechMovie in April of 2019.</li>
+        <li>As an online movie streaming and rental company, EZTechMovie offers a variety of flat-rate package subscriptions to its customers throughout the United States. It has a revenue of $1.5 million per year and currently employs 70 people.</li>
+        <li>Presently the company has about one million subscribers. It is estimated that, in the next three years, the number of customers will increase to 10 million.</li>
+      </ul>
     </div>
   );
 };
 
 export default About;
+
